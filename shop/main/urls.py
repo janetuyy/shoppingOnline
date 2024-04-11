5e4fa1fa1c65288a808c8c1d68c9de6d980256ff
janetuyy <118744357+janetuyy@users.noreply.github.com>
2024-04-11 15:36:45 +0300

    filtering by category

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
+
 urlpatterns = [
     path("", views.product_list, name="main"),
     path("cart/", views.cart, name="cart"),
     path("account/", views.account, name="account"),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
